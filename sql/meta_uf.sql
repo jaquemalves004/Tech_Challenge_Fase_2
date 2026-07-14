d83066dc0ba7313e4d3e475043da54784da9cff6
jaquemalves004 <jaquemalves004@gmail.com>
2026-07-14 14:06:19 -0300

    Create meta_uf.sql

---
+++ sql/meta_uf.sql
@@ -0,0 +1,15 @@
+SELECT
+    ano,
+    sigla_uf,
+    rede,
+    taxa_alfabetizacao,
+    meta_alfabetizacao_2024,
+    meta_alfabetizacao_2025,
+    meta_alfabetizacao_2026,
+    meta_alfabetizacao_2027,
+    meta_alfabetizacao_2028,
+    meta_alfabetizacao_2029,
+    meta_alfabetizacao_2030,
+    percentual_participacao
+FROM `basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_uf`
+WHERE ano >= 2023;
